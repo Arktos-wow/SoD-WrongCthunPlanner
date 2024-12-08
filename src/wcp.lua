56dc20748b988bf023a759952fa2a586d56e6bdd
Arktos-wow <titor.gaming@gmail.com>
2024-12-08 03:25:26 -0800

    Update wcp.lua

--- src/wcp.lua
+++ src/wcp.lua
@@ -1,70 +1,68 @@
-WCP = LibStub("AceAddon-3.0"):NewAddon("WCP", "AceComm-3.0", "AceSerializer-3.0")
-WCP.LIB = {}
+if not WCP then
+    WCP = LibStub("AceAddon-3.0"):NewAddon("WCP", "AceComm-3.0", "AceSerializer-3.0")
+    WCP.LIB = {}
 
-WCP.messagePrefix = "WCP"
-WCP.frame = {}
-WCP.player_name = UnitName("player")
+    WCP.messagePrefix = "WCP"
+    WCP.frame = {}
+    WCP.player_name = UnitName("player")
 
-function WCP.info(message)
-  DEFAULT_CHAT_FRAME:AddMessage("|cff00d2d6WCP:|r " .. message, 1.0, 1.0, 0)
-end
-
-function WCP.alert(message)
-  DEFAULT_CHAT_FRAME:AddMessage("|cFFFF0000WCP:|r " .. message, 1.0, 1.0, 0)
-end
-
-function WCP.submit_event(event, target)
-  if(target) then
-    WCP.addon_whisper_message(WCP:Serialize(event), target)
-  else
-    WCP.addon_raid_message(WCP:Serialize(event))
-  end
-end
-
-function WCP.addon_raid_message(message)
-  WCP:SendCommMessage(WCP.messagePrefix, message, "RAID")
-end
-
-function WCP.addon_whisper_message(message, target)
-  WCP:SendCommMessage(WCP.messagePrefix, message, "WHISPER", target)
-end
-
-function WCP.debug(object)
-  print(WCP.dump(object))
-end
-
-function WCP.dump(object)
-  if type(object) == 'table' then
-    local s = "{"
-    for k, v in pairs(object) do
-      if type(k) ~= 'number' then k = '"'..k..'"' end
-
-      s = s .. '['..k..'] = ' .. WCP.dump(v) .. ", "
+    function WCP.info(message)
+        DEFAULT_CHAT_FRAME:AddMessage("|cff00d2d6WCP:|r " .. tostring(message), 1.0, 1.0, 0)
     end
-    return s .. '}'
-  else
-    return tostring(object)
-  end
-end
 
--- @note Lua does not have a String#split method implemented,
---       but WoW does. We just delegate here.
-function WCP.split(...)
-  strsplit(...)
-end
-
--- @see http://lua-users.org/wiki/CopyTable
-function WCP.copy_table(orig)
-  local orig_type = type(orig)
-  local copy = {}
-
-  if orig_type == 'table' then
-    for orig_key, orig_value in pairs(orig) do
-      copy[WCP.copy_table(orig_key)] = WCP.copy_table(orig_value)
+    function WCP.alert(message)
+        DEFAULT_CHAT_FRAME:AddMessage("|cFFFF0000WCP:|r " .. tostring(message), 1.0, 1.0, 0)
     end
-    setmetatable(copy, WCP.copy_table(getmetatable(orig)))
-  else -- number, string, boolean, etc
-    copy = orig
-  end
-  return copy
+
+    function WCP.submit_event(event, target)
+        local serialized_event = WCP:Serialize(event)
+        if target then
+            WCP.addon_whisper_message(serialized_event, target)
+        else
+            WCP.addon_raid_message(serialized_event)
+        end
+    end
+
+    function WCP.addon_raid_message(message)
+        WCP:SendCommMessage(WCP.messagePrefix, message, "RAID")
+    end
+
+    function WCP.addon_whisper_message(message, target)
+        WCP:SendCommMessage(WCP.messagePrefix, message, "WHISPER", target)
+    end
+
+    function WCP.debug(object)
+        print(WCP.dump(object))
+    end
+
+    function WCP.dump(object)
+        if type(object) == "table" then
+            local result = "{"
+            for k, v in pairs(object) do
+                local key = (type(k) == "number") and k or '"' .. k .. '"'
+                result = result .. "[" .. key .. "] = " .. WCP.dump(v) .. ", "
+            end
+            return result .. "}"
+        else
+            return tostring(object)
+        end
+    end
+
+    function WCP.split(...)
+        return strsplit(...)
+    end
+
+    function WCP.copy_table(orig)
+        if type(orig) ~= "table" then
+            return orig
+        end
+
+        local copy = {}
+        for orig_key, orig_value in pairs(orig) do
+            copy[WCP.copy_table(orig_key)] = WCP.copy_table(orig_value)
+        end
+        return setmetatable(copy, WCP.copy_table(getmetatable(orig)))
+    end
+else
+    print("WCP addon is already initialized.")
 end
